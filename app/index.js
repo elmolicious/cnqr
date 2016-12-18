25000a05edf8ae8d602c35c98cfdc860ec005728
Robin Genske <robin.genske@gmail.com>
2016-12-18 01:53:53 +0100

    Added first dummy assets, loaded them -> added them as a sprite and made a basic gameloop

--- app/index.js
+++ app/index.js
@@ -1,12 +1,44 @@
-var PIXI = require('pixi.js');
+const PIXI = require('pixi.js');
 
-//var renderer = PIXI.autoDetectRenderer(window.height, window.width);
-var renderer = PIXI.autoDetectRenderer(300, 300);
+const Container = PIXI.Container;
+const loader = PIXI.loader;
+const resources = PIXI.loader.resources;
+const Sprite = PIXI.Sprite;
 
-document
-	.body
-	.appendChild(renderer.view);
+let renderer;
+let stage;
 
-var stage = new PIXI.Container();
+let capture_point;
 
-renderer.render(stage);
+loader
+	.add(["assets/capture_points/capture_point_blue.png", "assets/capture_points/capture_point_red.png", "assets/capture_points/capture_point_neutral.png"])
+	.load(setup);
+
+function setup() {
+	renderer = PIXI.autoDetectRenderer(300, 300);
+
+	document
+		.body
+		.appendChild(renderer.view);
+
+	stage = new Container();
+
+	capture_point = new Sprite(resources["assets/capture_points/capture_point_blue.png"].texture);
+	capture_point.x = 0;
+	capture_point.y = 0;
+	stage.addChild(capture_point);
+
+	gameLoop();
+}
+
+function gameLoop() {
+
+	//Loop this function 60 times per second
+	requestAnimationFrame(gameLoop);
+
+	capture_point.x++;
+	//Move the cat 1 pixel per frame
+
+	//Render the stage
+	renderer.render(stage);
+}
